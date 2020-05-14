b688f2ed05573b71de16ad01919f2f4db17ee8d9
Hiroshi Nakagawa <kurokirika128@gmail.com>
2020-05-14 15:51:21 +0900

    Routing of /home has commentouted to web.php.

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'ArticleController@index');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
