ca92d3548d61b2e64937f136c5393f03edd93987
Hiroshi Nakagawa <rosshi96@gmail.com>
2020-05-14 03:58:48 +0000

    Article model data has readed to ArticleController.php.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
+
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -10,40 +12,7 @@ class ArticleController extends Controller
     public function index()
     {
         // ダミーデータ
-        $articles = [
-            (object) [
-                'id' => 1,
-                'title' => 'タイトル1',
-                'body' => '本文1',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 1,
-                    'name' => 'ユーザー名1'
-                ],
-            ],
-
-            (object) [
-                'id' => 2,
-                'title' => 'タイトル2',
-                'body' => '本文2',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 2,
-                    'name' => 'ユーザー名2'
-                ],
-            ],
-
-            (object) [
-                'id' => 3,
-                'title' => 'タイトル3',
-                'body' => '本文3',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 3,
-                    'name' => 'ユーザー名3'
-                ],
-            ],
-        ];
+        $articles = Article::all()->sortByDesc('created_at');
 
         // トップページ(記事一覧)を表示
         return view('articles.index', ['articles' => $articles]);
