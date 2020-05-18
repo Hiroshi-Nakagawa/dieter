c0eb102d55f3bb5e52c104102513862c84f1cf14
Hiroshi Nakagawa <rosshi96@gmail.com>
2020-05-18 04:16:11 +0000

    Login & logout test has added to ExampleTest.php.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -18,9 +19,8 @@ class ExampleTest extends TestCase
         $this->withoutExceptionHandling();
         $response = $this->get('/');
 
-        $response->assertStatus(500);
-        // $response->assertStatus(200);
-                // ->assertViewIs('articles.index');
+        $response->assertStatus(200)
+                ->assertViewIs('articles.index');
     }
 
     // ユーザー登録画面の表示確認
@@ -31,5 +31,33 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200)
                 ->assertViewIs('auth.register');
+    }
+
+    // ログイン
+    public function testLoginView()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this
+                    ->actingAs(User::find(1))
+                    ->get('/');
+
+        $response->assertStatus(200)
+                ->assertSee('投稿する');
+    }
+
+    // ログアウト
+    public function testLogoutView()
+    {
+        $this->withoutExceptionHandling();
+        // ログイン状態でログアウトしたら…
+        $response = $this
+                    ->actingAs(User::find(1))
+                    ->post(route('logout'));
+        
+        // 正常にリダイレクトされているか
+        $response->assertRedirect('/');
+
+        // ユーザーが認証されていないことを確認
+        $this->assertGuest();
     }
 }
