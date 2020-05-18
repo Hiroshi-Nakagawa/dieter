2136d2ec31a715f7bff385b22ccb972dfe76219e
Hiroshi Nakagawa <rosshi96@gmail.com>
2020-05-18 01:02:29 +0000

    Articles CRUD routing has added to web.php.

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
-Route::get('/', 'ArticleController@index');
 Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'ArticleController@index')->name('articles.index');
+Route::resource('/articles', 'ArticleController')->except(['index']);
