0c33293fcd8ad1b65a78fd64e5232241217c187b
Hiroshi Nakagawa <kurokirika128@gmail.com>
2020-08-20 10:05:18 +0900

    Creation of user page and implementation of screen transition are completed.

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
